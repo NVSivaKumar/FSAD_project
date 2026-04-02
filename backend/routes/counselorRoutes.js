ebfc3479d7b4eb1c0d0b9c228671f5d508682aa1
NVSivaKumar <nvedesh88@gmail.com>
2026-04-02 14:12:28 +0530

    commited

--- backend/routes/counselorRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getCounselors } from '../controllers/counselorController.js';
-
-const router = express.Router();
-
-// GET all counselors
-router.get('/', getCounselors);
-
-export default router;
